caed44f54af62c2b030530aae66356c6a6b798da
Talmiz Ahmed <talmiz.tech@gmail.com>
2025-07-02 17:15:48 +0530

    test fixes

--- test/unit/customComponent.test.js
+++ test/unit/customComponent.test.js
@@ -1,5 +1,4 @@
 import path from 'path';
 import { testBasicMarkup } from './testUtils.js';
 
-window.hlx = { codeBasePath: '' };
-testBasicMarkup(path.resolve('./test/unit/fixtures/custom-component/range.js'), false, ['range'], '../../test/unit/fixtures');
+testBasicMarkup(path.resolve('./test/unit/fixtures/custom-component/range.js'), false, ['range'], '../..');
