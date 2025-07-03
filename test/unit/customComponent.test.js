6ccd55728135a0ac408b759f6a490f87ac26fd20
Talmiz Ahmed <talmiz.tech@gmail.com>
2025-07-03 11:32:06 +0530

    no test for custom

--- test/unit/customComponent.test.js
+++
@@ -1,4 +0,0 @@
-import path from 'path';
-import { testBasicMarkup } from './testUtils.js';
-
-testBasicMarkup(path.resolve('./test/unit/fixtures/custom-component/range.js'), false, ['range'], '../..');
