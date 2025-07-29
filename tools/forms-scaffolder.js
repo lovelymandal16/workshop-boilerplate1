e16313aee4fae01a037c1318d6db757067579a83
Talmiz Ahmed <talmiz.tech@gmail.com>
2025-07-29 22:58:30 +0530

    decorate with more params

--- tools/forms-scaffolder.js
+++ tools/forms-scaffolder.js
@@ -146,12 +146,16 @@ function createComponentFiles(componentName, baseComponent, targetDir) {
  * Decorates a custom form field component
  * @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper. Refer to the documentation for its structure for each component.
  * @param {Object} fieldJson - The form json object for the component.
+ * @param {HTMLElement} parentElement - The parent container element of the field.
+ * @param {string} formId - The unique identifier of the form.
  */
-export default async function decorate(fieldDiv, fieldJson) {
-  console.log('${emojis.gear} Decorating ${componentName} component:', fieldDiv, fieldJson);
+export default async function decorate(fieldDiv, fieldJson, parentElement, formId) {
+  console.log('${emojis.gear} Decorating ${componentName} component:', fieldDiv, fieldJson, parentElement, formId);
   
   // TODO: Implement your custom component logic here
   // You can access the field properties via fieldJson.properties
+  // You can access the parent container via parentElement
+  // You can access the form ID via formId
   
   return fieldDiv;
 }
