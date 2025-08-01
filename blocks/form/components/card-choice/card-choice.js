3d7c0357404d456b8db2f6e9288e0e6957eed467
Ravisanker E <rae@ravisankers-mbp.macromedia.com>
2025-08-01 15:57:40 +0530

    fix custom component image

--- blocks/form/components/card-choice/card-choice.js
+++ blocks/form/components/card-choice/card-choice.js
@@ -3,11 +3,11 @@ import { subscribe } from '../../rules/index.js';
 
 function createCard(element, enums) {
   element.querySelectorAll('.radio-wrapper').forEach((radioWrapper, index) => {
-    if(enums[index]?.name) {
+    if (enums[index]?.name) {
       radioWrapper.querySelector('label').textContent = enums[index]?.name;
     }
     radioWrapper.querySelector('input').dataset.index = index;
-    const image = createOptimizedPicture(enums[index].image || '/blocks/form/components/card-choice/card.png', 'card-image');
+    const image = createOptimizedPicture(enums[index].image || 'https://main--afb--jalagari.hlx.page/lab/images/card.png', 'card-image');
     const benefitsUl = document.createElement('ul');
     benefitsUl.className = 'card-choice-benefits-list';
     const benefits = enums[index]?.benefits?.split(',')?.map((b) => b.trim())?.filter(Boolean);
