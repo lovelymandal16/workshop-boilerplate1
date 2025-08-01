3164d5b12884bf31c19f5014473b0208a10437d4
Ravisanker E <rae@ravisankers-mbp.macromedia.com>
2025-08-01 14:18:36 +0530

    fix createOptimisedPicture for absolute urls

--- scripts/aem.js
+++ scripts/aem.js
@@ -302,16 +302,18 @@ function createOptimizedPicture(
   breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }],
 ) {
   const url = new URL(src, window.location.href);
+  const isSameOrigin = url.origin === window.location.origin;
+  const optimizedSrc = isSameOrigin ? url.pathname : url.origin + url.pathname;
   const picture = document.createElement('picture');
-  const { pathname } = url;
-  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+  // const { pathname } = url;
+  const ext = optimizedSrc.substring(optimizedSrc.lastIndexOf('.') + 1);
 
   // webp
   breakpoints.forEach((br) => {
     const source = document.createElement('source');
     if (br.media) source.setAttribute('media', br.media);
     source.setAttribute('type', 'image/webp');
-    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+    source.setAttribute('srcset', `${optimizedSrc}?width=${br.width}&format=webply&optimize=medium`);
     picture.appendChild(source);
   });
 
@@ -320,14 +322,14 @@ function createOptimizedPicture(
     if (i < breakpoints.length - 1) {
       const source = document.createElement('source');
       if (br.media) source.setAttribute('media', br.media);
-      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      source.setAttribute('srcset', `${optimizedSrc}?width=${br.width}&format=${ext}&optimize=medium`);
       picture.appendChild(source);
     } else {
       const img = document.createElement('img');
       img.setAttribute('loading', eager ? 'eager' : 'lazy');
       img.setAttribute('alt', alt);
       picture.appendChild(img);
-      img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      img.setAttribute('src', `${optimizedSrc}?width=${br.width}&format=${ext}&optimize=medium`);
     }
   });
 
